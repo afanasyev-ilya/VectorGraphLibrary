e0459f62e18dfc9fb030f463769768c2ada15a54
Elijah <afanasiev_ilya@icloud.com>
2020-04-27 14:05:49 +0300

    makefile.intel updated - c++11 flags - compilation with GPU

--- graph_processing_API/gpu/graph_primitives/compute.cu
+++ graph_processing_API/gpu/graph_primitives/compute.cu
@@ -2,37 +2,6 @@
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-template <typename _TVertexValue, typename _TEdgeWeight, typename ComputeOperation>
-void GraphPrimitivesGPU::compute(ExtendedCSRGraph<_TVertexValue, _TEdgeWeight> &_graph,
-                                 FrontierGPU &_frontier,
-                                 ComputeOperation &&compute_op)
-{
-    LOAD_EXTENDED_CSR_GRAPH_DATA(_graph);
-
-    long long *vertex_pointers = outgoing_ptrs;
-
-    if(_frontier.type == ALL_ACTIVE_FRONTIER)
-    {
-        SAFE_KERNEL_CALL((compute_kernel_all_active <<< (vertices_count - 1) / BLOCK_SIZE + 1, BLOCK_SIZE >>>
-                                          (vertices_count, vertex_pointers, compute_op)));
-    }
-    else if(_frontier.type == DENSE_FRONTIER)
-    {
-        SAFE_KERNEL_CALL((compute_kernel_dense <<< (vertices_count - 1) / BLOCK_SIZE + 1, BLOCK_SIZE >>>
-                                          (_frontier.flags, vertices_count, vertex_pointers, compute_op)));
-    }
-    else if(_frontier.type == SPARSE_FRONTIER)
-    {
-        int frontier_size = _frontier.size();
-        SAFE_KERNEL_CALL((compute_kernel_sparse <<< (frontier_size - 1) / BLOCK_SIZE + 1, BLOCK_SIZE >>>
-                                          (_frontier.ids, frontier_size, vertex_pointers, compute_op)));
-    }
-
-    cudaDeviceSynchronize();
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 template<typename ComputeOperation>
 void __global__ compute_kernel_all_active(const int _frontier_size,
                                           const long long *_vertex_pointers,
@@ -83,3 +52,35 @@ void __global__ compute_kernel_sparse(const int *_frontier_ids,
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+template <typename _TVertexValue, typename _TEdgeWeight, typename ComputeOperation>
+void GraphPrimitivesGPU::compute(ExtendedCSRGraph<_TVertexValue, _TEdgeWeight> &_graph,
+                                 FrontierGPU &_frontier,
+                                 ComputeOperation &&compute_op)
+{
+    LOAD_EXTENDED_CSR_GRAPH_DATA(_graph);
+
+    long long *vertex_pointers = outgoing_ptrs;
+
+    if(_frontier.type == ALL_ACTIVE_FRONTIER)
+    {
+        SAFE_KERNEL_CALL((compute_kernel_all_active <<< (vertices_count - 1) / BLOCK_SIZE + 1, BLOCK_SIZE >>>
+                (vertices_count, vertex_pointers, compute_op)));
+    }
+    else if(_frontier.type == DENSE_FRONTIER)
+    {
+        SAFE_KERNEL_CALL((compute_kernel_dense <<< (vertices_count - 1) / BLOCK_SIZE + 1, BLOCK_SIZE >>>
+                (_frontier.flags, vertices_count, vertex_pointers, compute_op)));
+    }
+    else if(_frontier.type == SPARSE_FRONTIER)
+    {
+        int frontier_size = _frontier.size();
+        SAFE_KERNEL_CALL((compute_kernel_sparse <<< (frontier_size - 1) / BLOCK_SIZE + 1, BLOCK_SIZE >>>
+                (_frontier.ids, frontier_size, vertex_pointers, compute_op)));
+    }
+
+    cudaDeviceSynchronize();
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
--- graph_processing_API/gpu/graph_primitives/generate_new_frontier.cu
+++ graph_processing_API/gpu/graph_primitives/generate_new_frontier.cu
@@ -24,23 +24,6 @@ struct is_not_active
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-template <typename _TVertexValue, typename _TEdgeWeight, typename Condition>
-void GraphPrimitivesGPU::generate_new_frontier(ExtendedCSRGraph<_TVertexValue, _TEdgeWeight> &_graph,
-                                               FrontierGPU &_frontier,
-                                               Condition &&cond)
-{
-    int vertices_count = _graph.get_vertices_count();
-    _frontier.type = SPARSE_FRONTIER;
-
-    SAFE_KERNEL_CALL((copy_frontier_ids_kernel<<<(vertices_count-1)/BLOCK_SIZE+1, BLOCK_SIZE>>>(_frontier.ids, _frontier.flags,
-            vertices_count, cond)));
-
-    int *new_end = thrust::remove_if(thrust::device, _frontier.ids, _frontier.ids + vertices_count, is_not_active());
-    _frontier.current_size = new_end - _frontier.ids;
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 template <typename Condition>
 void __global__ copy_frontier_ids_kernel(int *_frontier_ids,
                                          int *_frontier_flags,
@@ -61,6 +44,23 @@ void __global__ copy_frontier_ids_kernel(int *_frontier_ids,
             _frontier_flags[idx] = NOT_IN_FRONTIER_FLAG;
         }
     }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+template <typename _TVertexValue, typename _TEdgeWeight, typename Condition>
+void GraphPrimitivesGPU::generate_new_frontier(ExtendedCSRGraph<_TVertexValue, _TEdgeWeight> &_graph,
+                                               FrontierGPU &_frontier,
+                                               Condition &&cond)
+{
+    int vertices_count = _graph.get_vertices_count();
+    _frontier.type = SPARSE_FRONTIER;
+
+    SAFE_KERNEL_CALL((copy_frontier_ids_kernel<<<(vertices_count-1)/BLOCK_SIZE+1, BLOCK_SIZE>>>(_frontier.ids, _frontier.flags,
+                                                                                                vertices_count, cond)));
+
+    int *new_end = thrust::remove_if(thrust::device, _frontier.ids, _frontier.ids + vertices_count, is_not_active());
+    _frontier.current_size = new_end - _frontier.ids;
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
