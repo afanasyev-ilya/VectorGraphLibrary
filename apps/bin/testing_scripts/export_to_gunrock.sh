fa36b877697871f38a038435ad45876c09888e95
Elijah <afanasiev_ilya@icloud.com>
2020-07-03 11:41:19 +0300

    - NEC PR performance improvements (via register API) - still no packing in NEC PR - several architectures.h file features moved to apps for better tunning - testing script export to gunrock improvements (friendster graph, uncommented)

--- apps/bin/testing_scripts/export_to_gunrock.sh
+++ apps/bin/testing_scripts/export_to_gunrock.sh
@@ -16,14 +16,15 @@ mkdir gunrock_graphs
 ./generate_test_data -push -directed -s 24 -e 32 -type ru -file ./gunrock_graphs/ru_24_32 -format mtx
 ./generate_test_data -push -directed -s 25 -e 32 -type ru -file ./gunrock_graphs/ru_25_32 -format mtx
 
-#./generate_test_data  -directed -push -convert ./source_graphs/orkut-links/out.orkut-links -file ./gunrock_graphs/orkut -format mtx
-#./generate_test_data  -directed -push -convert ./source_graphs/dbpedia-link/out.dbpedia-link -file ./gunrock_graphs/dbpedia -format mtx
-#./generate_test_data  -directed -push -convert ./source_graphs/soc-LiveJournal1/out.soc-LiveJournal1 -file ./gunrock_graphs/lj -format mtx
-#./generate_test_data  -directed -push -convert ./source_graphs/soc-pokec-relationships/out.soc-pokec-relationships -file ./gunrock_graphs/pokec -format mtx
+./generate_test_data  -directed -push -convert ./source_graphs/orkut-links/out.orkut-links -file ./gunrock_graphs/orkut -format mtx
+./generate_test_data  -directed -push -convert ./source_graphs/dbpedia-link/out.dbpedia-link -file ./gunrock_graphs/dbpedia -format mtx
+./generate_test_data  -directed -push -convert ./source_graphs/soc-LiveJournal1/out.soc-LiveJournal1 -file ./gunrock_graphs/lj -format mtx
+./generate_test_data  -directed -push -convert ./source_graphs/soc-pokec-relationships/out.soc-pokec-relationships -file ./gunrock_graphs/pokec -format mtx
 
-#./generate_test_data  -directed -push -convert ./source_graphs/wikipedia_link_en/out.wikipedia_link_en  -file ./gunrock_graphs/wiki_en -format mtx
+./generate_test_data  -directed -push -convert ./source_graphs/wikipedia_link_en/out.wikipedia_link_en  -file ./gunrock_graphs/wiki_en -format mtx
 ./generate_test_data  -directed -push -convert ./source_graphs/wikipedia_link_fr/out.wikipedia_link_fr -file ./gunrock_graphs/wiki_fr -format mtx
-#./generate_test_data  -directed -push -convert ./source_graphs/trackers-trackers/out.trackers -file ./gunrock_graphs/trackers -format mtx
-#./generate_test_data  -directed -push -convert ./source_graphs/wikipedia_link_ru/out.wikipedia_link_ru -file ./gunrock_graphs/wiki_ru -format mtx
-#./generate_test_data  -directed -push -convert ./source_graphs/twitter/out.twitter  -file ./gunrock_graphs/twitter -format mtx
+./generate_test_data  -directed -push -convert ./source_graphs/trackers-trackers/out.trackers -file ./gunrock_graphs/trackers -format mtx
+./generate_test_data  -directed -push -convert ./source_graphs/wikipedia_link_ru/out.wikipedia_link_ru -file ./gunrock_graphs/wiki_ru -format mtx
+./generate_test_data  -directed -push -convert ./source_graphs/twitter/out.twitter  -file ./gunrock_graphs/twitter -format mtx
+./generate_test_data  -directed -push -convert ./source_graphs/friendster/out.friendster  -file ./gunrock_graphs/friendster -format mtx
 
