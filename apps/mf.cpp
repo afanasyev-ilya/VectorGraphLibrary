e0514a242e0c9af5a90fe6f5b1dec7932fa2f575
Elijah <afanasiev_ilya@icloud.com>
2020-07-03 14:01:12 +0300

    - MF problem template in .cpp file and make

---
+++ apps/mf.cpp
@@ -0,0 +1,55 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#define INT_ELEMENTS_PER_EDGE 4.0
+#define NEC_VECTOR_CORE_THRESHOLD_VALUE VECTOR_LENGTH
+#define COLLECTIVE_FRONTIER_TYPE_CHANGE_THRESHOLD 0.15
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "../graph_library.h"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main(int argc, const char * argv[])
+{
+    try
+    {
+        // parse args
+        AlgorithmCommandOptionsParser parser;
+        parser.parse_args(argc, argv);
+
+        // load graph
+        ExtendedCSRGraph<int, float> graph;
+        EdgesListGraph<int, float> rand_graph;
+        if(parser.get_compute_mode() == GENERATE_NEW_GRAPH)
+        {
+            int vertices_count = pow(2.0, parser.get_scale());
+            long long edges_count = vertices_count * parser.get_avg_degree();
+            GraphGenerationAPI<int, float>::R_MAT(rand_graph, vertices_count, edges_count, 57, 19, 19, 5, UNDIRECTED_GRAPH);
+            graph.import_graph(rand_graph);
+        }
+        else if(parser.get_compute_mode() == LOAD_GRAPH_FROM_FILE)
+        {
+            double t1 = omp_get_wtime();
+            if(!graph.load_from_binary_file(parser.get_graph_file_name()))
+                throw "ERROR: graph file not found";
+            double t2 = omp_get_wtime();
+            cout << "file " << parser.get_graph_file_name() << " loaded in " << t2 - t1 << " sec" << endl;
+        }
+
+        cout << "MF template" << endl;
+
+        //pr_operation.nec_page_rank(graph, page_ranks, 1.0e-4, iterations_count)uil
+    }
+    catch (string error)
+    {
+        cout << error << endl;
+    }
+    catch (const char * error)
+    {
+        cout << error << endl;
+    }
+    return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
