8f33b9968a6f7da8df2aa3ca5a26503d9c95ce54
Elijah <afanasiev_ilya@icloud.com>
2021-04-01 20:41:49 +0300

    - no print

--- apps/sssp/sssp.cpp
+++ apps/sssp/sssp.cpp
@@ -52,12 +52,13 @@ int main(int argc, const char * argv[])
         cout << "Computations started..." << endl;
         cout << "Doing " << parser.get_number_of_rounds() << " SSSP iterations..." << endl;
         EdgesArray_Vect<float> weights(graph);
-        weights.set_all_random(1.0);
-        weights.print();
-        //weights.set_all_constant(1.0);
+        //weights.set_all_random(1.0);
+        //weights.print();
+        weights.set_all_constant(1.0);
         for(int i = 0; i < parser.get_number_of_rounds(); i++)
         {
             int source_vertex = graph.select_random_vertex(ORIGINAL);
+            cout << "selected source vertex " << source_vertex << endl;
             VerticesArray<float> distances(graph, Parser::convert_traversal_type(parser.get_traversal_direction()));
 
             performance_stats.reset_timers();
